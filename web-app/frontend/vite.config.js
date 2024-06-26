256021e3de4b98df27b21d6d04933725171ec3b4
Ylwbro <jeremydan.lhomme@gmail.com>
2024-06-26 15:39:43 +0200

    update vite config

--- web-app/frontend/vite.config.js
+++ web-app/frontend/vite.config.js
@@ -12,27 +12,6 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-        },
-      },
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-      "/uploads": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/uploads/, ""),
-      },
-    },
   },
   resolve: {
     alias: {
