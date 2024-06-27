30205d81aceaa0de9c98ad2273be0c5171d764c1
Ylwbro <jeremydan.lhomme@gmail.com>
2024-06-27 17:32:57 +0200

    update vercel json

--- web-app/frontend/vite.config.js
+++ web-app/frontend/vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": process.env,
+  },
   build: {
     outDir: "dist",
   },
-  server: {
-    port: 5173,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
 });
